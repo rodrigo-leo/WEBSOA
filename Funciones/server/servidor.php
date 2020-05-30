853a4f8b0332eb44f4b87b8dc8786b896a2df75e
jonathan <jonathancggs@gmail.com>
2020-05-29 20:59:51 -0500

    update

---
+++ Funciones/server/servidor.php
@@ -0,0 +1,9 @@
+<?php
+    /*include __DIR__."/../../nusoap/lib/nusoap.php";
+
+    $miRUL = "http://localhost/SOA/Tienda/PHP/servidor.php";
+    $server = new soap_server();
+    $server->configureWSDL('WSDLTST', $miRUL);
+    $server->wsdl->schemaTargetNamespace=$miRUL;*/
+
+    print_r(__DIR__);
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+    header("Location: html/Login.html");
