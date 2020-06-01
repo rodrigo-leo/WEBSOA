9f60b591c56614f4f96a34f8a7020ce8110fa075
Jonathan <jonathancggs@gmail.com>
2020-05-31 19:29:56 -0500

    inicio
    se agrega el footer y el listado de productos

--- html/Inicio.php
+++ html/Inicio.php
@@ -1,4 +1,7 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+<link rel="stylesheet" href="../CSS/bootstrap.css">
+<script src="../JS/jquery.js"></script>
+<script src="../JS/bootstrap.js"></script>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">itqNet</a>
@@ -8,7 +11,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="http://localhost/dashboard/itqNet/html/Inicio.html">Inicio <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="http://localhost/dashboard/itqNet/html/Inicio.php">Inicio <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="http://localhost/dashboard/itqNet/html/AltaProducto.html"> Subir Productos</a>
@@ -16,6 +19,13 @@
       <li class="nav-item">
         <a class="nav-link" href="http://localhost/dashboard/itqNet/html/BajaProducto.html"> Bajar Productos</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/dashboard/itqNet/html/ListaProducto.html">Ver Productos</a>
+      </li>
     </ul>
   </div>
 </nav>
+
+<?php
+  include "footer.php";
+?>
