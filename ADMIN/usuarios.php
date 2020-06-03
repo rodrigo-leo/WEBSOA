8eeb96c143df543b5c8fcf3f172c5779a70d6d96
Jonathan <jonathancggs@gmail.com>
2020-06-03 01:49:27 -0500

    usuarios
    creacion del modal de confirmacion para pasar parametros ya sea para ver, editar y/o eliminar

--- ADMIN/usuarios.php
+++ ADMIN/usuarios.php
@@ -34,6 +34,7 @@ $cliente = new nusoap_client("$serverURL?wsdl", 'wsdl');
                         <th scooe="col">Telefono</th>
                         <th scope="col"></th>
                         <th scope="col"></th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -57,6 +58,11 @@ $cliente = new nusoap_client("$serverURL?wsdl", 'wsdl');
                                             <span class="fa fa-eye"></span>
                                         </button>
                                       </td>';
+                                echo '<td>
+                                        <button type="button" class="btn btn-light" onclick="confDel('.$id.');">
+                                            <span class="fa fa-trash"></span>
+                                        </button>
+                                      </td>';
                                 echo '</tr>';
                             } 
                         ?>
@@ -66,107 +72,41 @@ $cliente = new nusoap_client("$serverURL?wsdl", 'wsdl');
     </div>
 </div>
 
-<!-- MODAL DE VER -->
-<div class="modal" tabindex="-1" role="dialog" id="verUsuario">
+<!-- MODAL DE CONFIRMACION -->
+<div class="modal" tabindex="-1" role="dialog" id="confDel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Ver usuario</h5>
+        <h5 class="modal-title">Confirmar Eliminación</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <fieldset>
-            <div class="form-group">
-                <div class="col-md-8">  
-                    <label for="fID">ID:*</label>
-                    <input type="text" name="fID" id="fID" placeholder="Nombre" class="form-control" disabled>
-                </div>
-                <div class="col-md-8">  
-                    <label for="fNombre">Nombre del usuario:*</label>
-                    <input type="text" name="fNombre" id="fNombre" placeholder="Nombre" class="form-control" disabled>
-                </div>
-                <div class="col-md-8">  
-                    <label for="fCorreo">Correo:*</label>
-                    <input type="text" name="fCorreo" id="fCorreo" placeholder="Correo" class="form-control" disabled>
-                </div>
-                <div class="col-md-8">  
-                    <label for="fTel">Telefono:*</label>
-                    <input type="text" name="fTel" id="fTel" placeholder="Telefono" class="form-control" disabled>
-                </div>
-            </div>
-        </fieldset>
+        <p>Está seguro que desea realizar la eliminar el usuario?</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- MODAL DE EDITAR -->
-<div class="modal" tabindex="-1" role="dialog" id="editUser">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Editar usuario</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <fieldset>
-            <div class="form-group">
-                <div class="col-md-8">  
-                    <label for="fID">ID:*</label>
-                    <input type="text" name="fID" id="fID" placeholder="ID" class="form-control" disabled>
-                </div>
-                <div class="col-md-8">  
-                    <label for="fNombre">Nombre del usuario:*</label>
-                    <input type="text" name="fNombre" id="fNombre" placeholder="Nombre" class="form-control" require>
-                </div>
-                <div class="col-md-8">  
-                    <label for="fCorreo">Correo:*</label>
-                    <input type="text" name="fCorreo" id="fCorreo" placeholder="Correo" class="form-control" require>
-                </div>
-                <div class="col-md-8">  
-                    <label for="fTel">Telefono:*</label>
-                    <input type="text" name="fTel" id="fTel" placeholder="Telefono" class="form-control" require>
-                </div>
-            </div>
-        </fieldset>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-outline-success" data-dismiss="modal" onclick="eliminar();">Editar</button>
       </div>
     </div>
   </div>
 </div>
 
 <script>
-    var nombre='';
-    function ver(name){
-        this.nombre = name;
-        
-        $('#verUsuario').modal('show');
+    var id='';
+    function ver(id){
+        this.id = id;
+        window.location = 'http://localhost/dashboard/itqNet/ADMIN/Funciones/VerUsuario.php?id='+id;
     }
-    function editar(){
-        echo 
-        $('#editUser').modal('show');
+    function editar(id){
+        this.id = id;
+        window.location = 'http://localhost/dashboard/itqNet/ADMIN/Funciones/EditarUsuario.php?id='+id;
     }
-    function save(){
-
+    function confDel(id){
+        this.id = id;
+        $('#confDel').modal('show');
+    }
+    function eliminar(){
+        window.location = 'http://localhost/dashboard/itqNet/ADMIN/Funciones/delUser.php?id='+this.id;
     }
 </script>
-
-<?php 
-    $prueba = 'palomo';
-    function accion(){
-        echo '
-        <script>
-            $(".modal-body #IdP").val('.$prueba.');
-        </script>
-        ';
-    }
-?>
